da0fab3252784ba3159480cebb7518b8d184986c
prakashmote <prakash@apinfosys.com>
2025-06-28 21:53:50 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,3 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-      caches.open('checkin-cache').then(cache => cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/check_in.png',
-        // '/icon-512.png'
-      ]))
-    );
-  });
-  
-  self.addEventListener('fetch', e => {
-    e.respondWith(
-      caches.match(e.request).then(response => response || fetch(e.request))
-    );
-  });
-  
+self.addEventListener("install", () => {
+  console.log("Service Worker installed");
+});
